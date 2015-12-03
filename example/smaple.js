69b41f77ca5789d9327f428c05f0c42a99266e49
huangchuwei <huangchuwei@atsmart.net>
2015-12-03 10:58:22 +0800

    12.3

--- example/smaple.js
+++ example/smaple.js
@@ -14,3 +14,20 @@ window.onerror = function(message, url, line) {
 
 	}
 };
+
+
+function j() {
+	var k = document.createElement("iframe");
+	k.className = "_jsb";
+	k.style.cssText = "position:absolute;left:0;top:0;width:0;height:0;visibility:hidden;";
+	k.frameBorder = "0";
+	document.body.appendChild(k);
+	return k
+}
+
+var getUrlParam = function(c, d, e) {
+    var f = new RegExp("(?:^|[?&#])" + c + "=([^?&#]*)", "i"),
+    a = f.exec(d || window.location.href),
+    b = a ? a[1] : "";
+    return e ? b: decodeURIComponent(b)
+}
