b4778f2388e67c34b10b52ca6578c2940a613485
huangchuwei <huangchuwei@atsmart.net>
2016-06-22 16:00:48 +0800

    0622

---
+++ example/nodejs/europe_cup/config/development.js
@@ -0,0 +1,23 @@
+module.exports = {
+ 
+    wechat:{
+      id: 'xxx', 
+      secret: 'xxx'
+  },
+  redis:{
+      ip: '127.0.0.1',
+      port: 6379,
+      timeout: 1000
+  },
+ 
+   mysql:{
+       connectionLimit : 10, //important
+        host     : 'xxx',
+        user     : 'xxx',
+        password : 'xxx',
+        database : 'xxx',
+        debug    :  false
+  },
+  server_base:'http://xxx'
+  
+};
